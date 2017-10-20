5c31e2769c43e9b2ece126401873062a91fc1ca9
Angelo Viana <angelovianajr@gmail.com>
2017-10-20 15:05:20 -0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/services/location-service.js
@@ -0,0 +1,14 @@
+/**
+ * Return user lat and lng given CEP
+ *
+ * @param {String}  cep User CEP value
+ * @returns {{lat: Number, lng: Number}}
+ */
+function getLocationByCep() {
+
+}
+
+export default {
+  findByMobile,
+  listAll,
+};
