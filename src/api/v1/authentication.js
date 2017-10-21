362c0b2e74e8dfb083a9da0f23cc54908819960b
Calvin Gerling <calvingerling@outlook.com>
2017-10-20 23:02:10 -0200

    Adicionar endpoint para verificar se o usuário está logado

--- src/api/v1/authentication.js
+++ src/api/v1/authentication.js
@@ -3,9 +3,17 @@ import { Router } from 'express';
 import User from '../../models/user';
 import authService from '../../services/auth-service';
 import userService from '../../services/user-service';
-import locationService from '../../services/location-service';
 
 const router = Router();
+
+function verifyToken(req, res) {
+  const token = req.get('authorization');
+  authService.verifyToken(token).then(() => {
+    res.status(200).end();
+  }).catch((err) => {
+    res.status(401).json(err);
+  });
+}
 
 function login(req, res) {
   req.checkBody('mobile', 'Please, use a valid phone number').isLength({ min: 11 });
@@ -34,52 +42,8 @@ function login(req, res) {
   });
 }
 
-function register(req, res) {
-  req.checkBody('mobile', 'Phone cannot to be empty').notEmpty();
-  req.checkBody('mobile', 'Invalid phone number').isLength({ min: 11 });
-  req.checkBody('password', 'Password cannot be empty').notEmpty();
-  req.checkBody('cep', 'CEP cannot be empty').notEmpty();
-  req.checkBody('cep', 'Invalid CEP').isLength({ min: 8 });
-
-  const errors = req.validationErrors();
-  if (errors) {
-    return res.status(400).send(errors);
-  }
-
-  const { mobile, password, cep } = req.body;
-  const newUser = new User({ mobile, password, cep });
-
-  locationService.getLocationByCep(cep)
-    .then((coordinates) => {
-      if (coordinates != null) {
-        newUser.location = {
-          coordinates: [coordinates.lat, coordinates.lng],
-        };
-      }
-
-      return newUser;
-    }).then(user => new Promise((resolve, reject) => {
-      user.save((err) => {
-        if (err) reject(err);
-
-        resolve(newUser);
-      });
-    }))
-    .then(() => {
-      res.status(201).end();
-    })
-    .catch((error) => {
-      if (error.name === 'MongoError' && error.code === 11000) {
-        return res.status(400).json({ msg: 'Cellphone alredy exists' });
-      }
-
-      return res.status(500).send(error);
-    });
-
-  return undefined;
-}
-
-router.route('/signin').post(login);
-router.route('/signup').post(register);
+router.route('/auth')
+  .get(verifyToken)
+  .post(login);
 
 export default router;
